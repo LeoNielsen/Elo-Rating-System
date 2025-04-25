d1ad386fdad9366d5976351e93f168b83f92ee73
LeoNielsen <leo.nielsen21@gmail.com>
2025-04-25 13:12:06 +0200

    Keycloak setup for JWT

--- src/main/java/EloRatingSystem/Security/SecurityConfig.java
+++ src/main/java/EloRatingSystem/Security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +13,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // Define the JwtDecoder Bean
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8081/realms/demo-realm/protocol/openid-connect/certs")
+                .build();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,7 +29,9 @@ public class SecurityConfig {
                     auth.anyRequest().authenticated();
                 })
                 .oauth2Login(withDefaults())
+                .oauth2ResourceServer(oAuth -> oAuth.jwt(withDefaults()))
                 .build();
     }
+
 
 }
