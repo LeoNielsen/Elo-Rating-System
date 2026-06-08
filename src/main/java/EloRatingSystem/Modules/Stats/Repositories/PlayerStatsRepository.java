dfe540baabf9f53f9e696336e6df8a44f766b699
LeoNielsen <leo.nielsen21@gmail.com>
2026-06-08 19:49:45 +0200

    Refactor of classes and modules

--- src/main/java/EloRatingSystem/Modules/Stats/Repositories/PlayerStatsRepository.java
+++ src/main/java/EloRatingSystem/Modules/Stats/Repositories/PlayerStatsRepository.java
@@ -5,15 +5,17 @@ import EloRatingSystem.Modules.Stats.Models.PlayerStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
+@Repository
 public interface PlayerStatsRepository extends JpaRepository<PlayerStats, Long> {
    Optional<PlayerStats> findByPlayerId(Long PlayerId);
 
    @Query("""
-    SELECT new EloRatingSystem.Dtos.PlayerDtos.PlayerStatisticsResponseDto(p, ps, COALESCE(pds.ratingChange, 0))
+    SELECT new EloRatingSystem.Modules.Stats.Dtos.PlayerStatisticsResponseDto(p, ps, COALESCE(pds.ratingChange, 0))
     FROM Player p
     JOIN PlayerStats ps ON ps.player.id = p.id
     LEFT JOIN PlayerDailyStats pds ON pds.player.id = p.id AND pds.date = :today
